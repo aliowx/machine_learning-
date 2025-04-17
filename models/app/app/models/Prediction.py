585c32af775bc54451a47761748a5169b8f5eaee
alialibakhshi <alialibakhshi9898@gmail.com>
2025-04-17 07:14:38 +0100

    feat: add the prediction about the ML

--- models/app/app/models/Prediction.py
+++ models/app/app/models/Prediction.py
@@ -1,1 +1,36 @@
 from sqlalchemy import SQLColumnExpression
+from pydantic import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base_class import Base
+from sqlalchemy import (
+    DateTime,
+    Float,
+    ForeignKey,
+    Index,
+    Integer,
+    String,
+    func
+)
+
+class Prediction(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    
+    user_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )  
+    model_version_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey('ModelVersion.id'),
+        nullable=False
+    )
+    prediction_result: Mapped[str] = mapped_column(
+        String
+    )
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    
+    
