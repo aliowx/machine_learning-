e15fe3f2afe9fef47ef4f620d9f9473b150f1bf7
alialibakhshi <alialibakhshi9898@gmail.com>
2025-04-17 07:40:35 +0100

    feat: make the file about the feature to prediction

---
+++ models/app/app/models/InputFeature.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base_class import Base
+from sqlalchemy import (
+    DateTime,
+    Float,
+    ForeignKey,
+    Index,
+    Integer,
+    String,
+    func
+)
+
+
+class InputFeature(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    
+    prediction_id: Mapped[int] = mapped_column(
+        ForeignKey('Prediction.id', index=True)
+    )
+    feature_name: Mapped[str] = mapped_column(
+        String,
+        nullable=True
+    )
+    
+    feature_value: Mapped[str] = mapped_column(
+        String,
+        
+    )
