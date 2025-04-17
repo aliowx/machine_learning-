c74810f8131d69155b8f879d7561d2943c66c333
alialibakhshi <alialibakhshi9898@gmail.com>
2025-04-17 07:24:58 +0100

    feat: make the file Modelversion

---
+++ models/app/app/models/ModelVersion.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base_class import Base
+from sqlalchemy import (
+    DateTime,
+    Float,
+    ForeignKey,
+    Index,
+    Integer,
+    String,
+    func
+)
+
+
+
+class ModelVersion(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    
+    version_name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    
+    model_file_path: Mapped[str] = mapped_column(String, nullable=False)
+    
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
