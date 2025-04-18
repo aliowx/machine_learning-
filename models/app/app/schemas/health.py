537d40bb62b432cf74a8454d47975a1b768b3cc8
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-18 08:57:44 +0100

    feat: setup the health

---
+++ models/app/app/schemas/health.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class Status(BaseModel):
+    ok: bool = None
+    msg: str = None
+    time: float = None
+    
+    
+class Services(BaseModel):
+    redis: Status = Status()
+    postgres: Status = Status()
+    
+
+class HealthCheck(BaseModel):
+    ok: bool = True
+    package_name: str = "prediction"
+    app_version: str | None = None
+    commit_id: str | None = None
+    uptime: float | None = None
+    human_readable_uptime: str | None = None
+    services: Services = Services()
