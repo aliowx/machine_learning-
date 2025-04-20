deb2ab11bd0530082ddaf761971d971f003e3634
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-20 10:11:11 +0100

    feat: fix the crud log request

--- models/app/app/crud/crud_request_log.py
+++ models/app/app/crud/crud_request_log.py
@@ -6,3 +6,21 @@ from app.schemas.request_log import RequestLogCreate, RequestLogUpdate
 
 
 
+class CRUDRequestLog(CRUDBase[RequestLog, RequestLogCreate, RequestLogUpdate]):
+    
+    async def get_by_tracker_id(
+        self,
+        db: AsyncSession,
+        tracker_id: int | str
+    )-> RequestLog | None:
+        query = select(self.model).where(
+            and_(
+                self.model.tracker_id == tracker_id,
+                self.model.is_deleted.is_(None),
+            ),
+        )
+        response = await db.execute(query)
+        return response.scalar_one_or_none()
+    
+    
+request_log = CRUDRequestLog(RequestLog)
