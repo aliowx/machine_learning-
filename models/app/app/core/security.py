f8d6b1ef8e5bd1926b1900cbec74ecbd912f6abf
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-21 10:44:46 +0100

    feat: password hash

--- models/app/app/core/security.py
+++ models/app/app/core/security.py
@@ -17,3 +17,6 @@ def verify_password(plain_password:str, hashed_password:str)-> bool:
     return hashed_password == sha256(plain_password.encode()).hexdigest()
 
 
+
+def get_password_hash(password: str)-> str:
+    return sha256(password.encode()).hexdigest()
