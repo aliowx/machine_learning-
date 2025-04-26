09a25ebf0f1c80de86710de0a1fde49d4e0d40da
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-26 11:33:58 +0100

    feat: set cookies for  id user

--- models/app/app/api/deps.py
+++ models/app/app/api/deps.py
@@ -6,7 +6,7 @@ import redis.asyncio as redis
 from fastapi import Depends, Request, Response
 from redis.asyncio import client
 from sqlalchemy.ext.asyncio import AsyncSession
-from app import crud, schemas, models
+from app import crud, schemas, models, utils
 from app import exceptions as exc
 from app.utils import redis_client
  
@@ -35,3 +35,34 @@ async def get_redis() -> client.Redis:
     except Exception as e:
         logger.error(f"Redis connection failed\n{e}")
         raise e 
+    
+    
+    
+async def get_user_id_from_cookie(
+    request: Request,
+    response: Response,
+    cache: client.Redis = Depends(get_redis)
+):
+    try:
+        access_token = request.cookies.get('Access-Token')
+        
+        if not access_token:
+            
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.access_token_not_found
+            )  
+            
+            
+        token = JWTHandler.decode(access_token)
+        if await cache.get(ACCESS_TOKEN_BLOCKLIST_KEY.format(token=access_token)):
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.expired_token
+            )
+        
+        user_id = token.get('id')
+        if token.get('sub') != 'access' or not user_id:
+            raise exc.UnauthorizedException(
+                msg_code=utils.MessageCodes.invalid_token
+            )
+            
+    except :...
