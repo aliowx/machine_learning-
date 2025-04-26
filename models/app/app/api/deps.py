8bd8ee02facb4f58ce0f77cc87cb99080a1e0b94
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-26 11:18:57 +0100

    feat: get async redic and add logger

--- models/app/app/api/deps.py
+++ models/app/app/api/deps.py
@@ -6,8 +6,9 @@ import redis.asyncio as redis
 from fastapi import Depends, Request, Response
 from redis.asyncio import client
 from sqlalchemy.ext.asyncio import AsyncSession
-from app import crud, schemas, models, utils
+from app import crud, schemas, models
 from app import exceptions as exc
+from app.utils import redis_client
  
 from app.core.config import(
     settings,
@@ -17,9 +18,20 @@ from app.core.config import(
 from app.core.security import JWTHandler, basic_security
 from app.db.session import async_session
 
+logger = logging.getLogger(__name__)   
 
 async def get_db_async() -> AsyncGenerator:
     # get database///
 
     async with async_session() as session:
         yield session 
+        
+        
+async def get_redis() -> client.Redis:
+    try:
+        if await redis_client.ping():
+            return redis_client
+        raise redis.RedisError('ping Error')
+    except Exception as e:
+        logger.error(f"Redis connection failed\n{e}")
+        raise e 
