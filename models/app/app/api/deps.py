da065f0baa1d948929af26225ae288d25f8872f4
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-26 11:03:52 +0100

    feat: maek  dependency file

---
+++ models/app/app/api/deps.py
@@ -0,0 +1,19 @@
+import logging 
+import secrets
+from types import AsyncGeneratorType
+from fastapi.security import HTTPAuthorizationCredentials
+import redis.asyncio as redis
+from fastapi import Depends, Request, Response
+from redis.asyncio import client
+from sqlalchemy.ext.asyncio import AsyncSession
+from app import crud, schemas, models, utils
+from app import exceptions as exc
+ 
+from app.core.config import(
+    settings,
+    ACCESS_TOKEN_BLOCKLIST_KEY,
+    REFRESH_TOKEN_BLOCKLIST_KEY
+)
+from app.core.security import JWTHandler, basic_security
+from app.db.session import async_session
+
