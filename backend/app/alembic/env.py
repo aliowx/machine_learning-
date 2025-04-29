dc6a1b51d04f2b6f5ea2a5b50d624b4c581c1980
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-29 12:22:11 +0100

    feat: Fix the file

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -1,20 +1,9 @@
-import asyncio
-import json
 from logging.config import fileConfig
 
-from sqlalchemy import MetaData, pool
-from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from alembic import context
-
-from app.db.base_class import Base
-from app.db.parser import CustomJSONEncoder, CustomJSONDecoder
-from app.core.config import settings
-
-
-def get_url():
-    return str(settings.POSTGRES_ASYNC_URI)
-
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,19 +14,16 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", get_url())
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-rollback = int(context.get_x_argument(as_dictionary=True).get("rollback", "0"))
 
 
 def run_migrations_offline() -> None:
@@ -52,52 +38,38 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = get_url()
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        compare_type=True,
     )
 
     with context.begin_transaction():
         context.run_migrations()
 
 
-def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)  # type: ignore
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
 
-    with context.begin_transaction() as _:  # noqa
-        context.run_migrations()
-        live_meta = MetaData()
-        live_meta.reflect(connection)
-        if rollback:
-            connection.rollback()
-
-
-async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
+    In this scenario we need to create an Engine
     and associate a connection with the context.
+
     """
-    connectable = async_engine_from_config(
+    connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
-        json_serializer=lambda x: json.dumps(x, cls=CustomJSONEncoder),
-        json_deserializer=lambda x: json.loads(x, cls=CustomJSONDecoder),
     )
 
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
-    await connectable.dispose()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
-
-    asyncio.run(run_async_migrations())
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 if context.is_offline_mode():
