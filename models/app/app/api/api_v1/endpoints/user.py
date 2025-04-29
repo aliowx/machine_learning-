0652666918f86ff3f0bc8dcc2827d4c7f50e0cdb
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-29 06:50:11 +0100

    fix: make cache for endpoint user

--- models/app/app/api/api_v1/endpoints/user.py
+++ models/app/app/api/api_v1/endpoints/user.py
@@ -6,14 +6,21 @@ from app.app.api import deps
 from app.app.api.api_v1 import services
 from app.app.log import  log
 from app.app.utils import APIResponse, APIResponseType
-
-
-
+from app.app.cache import cache, invalidate
+from app.app.cache.util import ONE_DAY_IN_SECONDS
+from sqlalchemy.ext.asyncio import AsyncSession
 router = APIRouter()
 namespace = 'user'
 
 
 @router.get('/')
+@cache(namespace=namespace, expire=ONE_DAY_IN_SECONDS)
 async def read_users(
+    db: AsyncSession = Depends(deps.get_db_async),
+    skip: int = 0,
+    limit: int = 100,
+    _ : models.User = Depends(deps.get_current_superuser_from_cookie_or_basic),
+)-> APIResponseType[list[schemas.User]]:
     
-)-> APIResponseType[list[schemas.User]]:...
+    users = await crud.user.get_multi(db=db, skip=skip, limit=limit)
+    return APIResponse(users)
