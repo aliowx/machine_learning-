989b2ff3d9b10157a7740cc31ba6e533cecdf69e
alialibakhshi <aliowx@users.noreply.github.com>
2025-04-30 06:32:43 +0100

    fix: fix

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,17 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 
 from app.core.config import settings
 
-
-async_engine =  create_async_engine(
+async_engine = create_async_engine(
     settings.POSTGRES_ASYNC_URI.unicode_string()
 )
-
-
 
 async_session = async_sessionmaker(
     bind=async_engine,
     class_=AsyncSession,
     autocommit=False,
     autoflush=False,
-    expire_on_commit=False
+    expire_on_commit=False,
 )
