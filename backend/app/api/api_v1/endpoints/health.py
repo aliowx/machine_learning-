484366433500f7500b0097b6776c09a02b6f54bc
alialibakhshi <aliowx@users.noreply.github.com>
2025-05-01 09:02:18 +0100

    feat: check internal and external services

--- backend/app/api/api_v1/endpoints/health.py
+++ backend/app/api/api_v1/endpoints/health.py
@@ -18,3 +18,58 @@ def ping(_=Depends(health_user))-> bool:
     Return 'True'
     """
     return True
+
+
+
+
+@router.get("/check", response_model=schemas.HealthCheck)
+async def deep_check(_=Depends(health_user)) -> schemas.HealthCheck:
+    """
+    check internal and external services
+    """
+
+    health = schemas.HealthCheck()
+
+    health.app_version = settings.APP_VERSION
+    health.commit_id = settings.COMMIT_ID
+
+    health.uptime, health.human_readable_uptime = utils.get_linux_uptime()
+
+    # Redis and Cache
+    start = time.time()
+    try:
+        await get_redis()
+        health.services.redis.ok = True
+        health.services.redis.msg = "Redis is ok"
+        health.services.redis.time = time.time() - start
+
+    except Exception as e:
+        health.ok = False
+        health.services.redis.ok = False
+        health.services.redis.msg = f"ERROR: {str(type(e))}, {str(e)}"
+        health.services.redis.time = time.time() - start
+
+    # Postgres
+    start = time.time()
+    try:
+        async with session.async_session() as db:
+            user = await crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
+
+        if user:
+            health.services.postgres.ok = True
+            health.services.postgres.msg = f"first user found: {user.email}"
+            health.services.postgres.time = time.time() - start
+
+        else:
+            health.ok = False
+            health.services.postgres.ok = False
+            health.services.postgres.msg = "first user not found."
+            health.services.postgres.time = time.time() - start
+
+    except Exception as e:
+        health.ok = False
+        health.services.postgres.ok = False
+        health.services.postgres.msg = f"ERROR: {str(type(e))}, {str(e)}"
+        health.services.postgres.time = time.time() - start
+
+    return health
