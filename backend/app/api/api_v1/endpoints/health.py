95a90175b99a5ea4beeb8158814bd31276f702b2
alialibakhshi <aliowx@users.noreply.github.com>
2025-05-01 08:45:17 +0100

    feat: add router

--- backend/app/api/api_v1/endpoints/health.py
+++ backend/app/api/api_v1/endpoints/health.py
@@ -1,5 +1,5 @@
 import time 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.core.config import settings
 from app.api.deps import health_user, get_redis
 from app import schemas, crud
@@ -8,4 +8,13 @@ from app.db import session
 
 
 
+router = APIRouter()
 
+
+
+@router.get("/ping", response_model=bool)
+def ping(_=Depends(health_user))-> bool: 
+    """
+    Return 'True'
+    """
+    return True
