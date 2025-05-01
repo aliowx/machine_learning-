a56c45d84ca1e40905c7121fdc7914a96737419b
alialibakhshi <aliowx@users.noreply.github.com>
2025-05-01 08:46:22 +0100

    feat: fix router

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import auth, user
+from app.api.api_v1.endpoints import auth, user, health
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(health.router, prefix="/health", tags=["health"])
