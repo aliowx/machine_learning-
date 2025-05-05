30ba1dbd75ade7b546bd69c0490acd512eac15eb
alialibakhshi <aliowx@users.noreply.github.com>
2025-05-05 11:25:08 +0100

    feat: fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,10 +15,9 @@ from app.exceptions import exception_handlers
 from app.models import User
 from app.cache import Cache
 
-# تعریف مسیر پوشه build فرانت‌اند
 current_dir = os.path.dirname(__file__)
-frontend_build_dir = os.path.join(current_dir, "../../frontend/build") 
-static_dir = os.path.join(frontend_build_dir, "static")  
+static_dir = os.path.join(current_dir, "static")
+
 
 def init_logger():
     logger = logging.getLogger()
@@ -75,17 +74,5 @@ if settings.BACKEND_CORS_ORIGINS:
 
 if os.path.exists(static_dir):
     app.mount("/static", StaticFiles(directory=static_dir), name="static")
-
-
-@app.get("/{full_path:path}")
-async def serve_frontend(full_path: str):
-
-    file_path = os.path.join(frontend_build_dir, full_path)
-
-    if os.path.exists(file_path) and not os.path.isdir(file_path):
-        return FileResponse(file_path)
-
-    return FileResponse(os.path.join(frontend_build_dir, "index.html"))
-
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.add_middleware(GetAcceptLanguageMiddleware)
