8e64ec69bfa638e90b7fcc86d80ec3bb15c5e4b4
Martin <alialibakhshi9898@gmail.com>
2025-05-09 09:31:12 +0100

    feat:✅ make worker file

--- backend/app/celery/worker.py
+++ backend/app/celery/worker.py
@@ -1,6 +1,24 @@
-from app.core.celery_app import celery_app
+import logging 
+import math 
+import random
+from datetime import datetime, timedelta, UTC
+from app.core.celery_app import DatabaseTask, celery_app
 
 
-@celery_app.task(name="app.celery.worker.test_celery")
-def test_celery(word: str)-> str:
-    return f"test task {word}"
+
+namespace = "job worker"
+
+logger = logging.getLogger(__name__)
+
+
+@celery_app.task(
+    base=DatabaseTask,
+    bind=True,
+    acks_late=True,
+    max_retries=4,
+    soft_time_limit=240,
+    time_limit=360,
+    name="add_events",
+)
+def test(word: str)->str:
+    return f"task celery app {word}"
