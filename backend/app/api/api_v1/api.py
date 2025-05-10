5654953be43eb9b09795ffa8a4e3d3af1569a4b9
Martin <alialibakhshi9898@gmail.com>
2025-05-10 14:29:48 +0100

    feat:🏁 router fix

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import auth, user, health, utils
+from app.api.api_v1.endpoints import auth, user, health, utils, upload_csv
 
 api_router = APIRouter()
 
@@ -7,3 +7,4 @@ api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(health.router, prefix="/health", tags=["health"])
 api_router.include_router(utils.router, prefix='/utils', tags=["utils"])
+api_router.include_router(upload_csv.router, prefix='/upload_csv', tags=["upload_csv"])
