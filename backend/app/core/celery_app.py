9622e36bf0b567ba8b347424e2093c9aef93c0a2
Martin <alialibakhshi9898@gmail.com>
2025-05-10 08:14:44 +0100

    feat:⬆️ fit and add db celert app conf

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -1,15 +1,50 @@
 from celery import Celery, Task
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.scoping import scoped_session
+from sqlalchemy.orm.session import sessionmaker
+
 from app.core.config import settings
 
-BROKER_URL = str(settings.REDIS_URI)
-
-
 celery_app = Celery(
-    "worker", backend="rpc://", broker=BROKER_URL, include=["celery.worker"]
+    "worker",
+    backend="rpc://",
+    broker=str(settings.REDIS_URI),
+    include=["celery.worker"],
 )
 
-celery_app.conf.update(task_track_started=True, broker_connection_retry_on_startup=True)
+
+ScopedSession = None
+
+from celery.signals import worker_init
 
 
+@worker_init.connect
+def bootstrap(*args, **kwargs):
+    global ScopedSession
+    engine = create_engine(
+        str(settings.SQLALCHEMY_DATABASE_URI), pool_pre_ping=True
+    )
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    ScopedSession = scoped_session(SessionLocal)
 
-class DatabaseTask(Task):...
+
+class DatabaseTask(Task):
+    abstract = True
+
+    _session = None
+
+    def after_return(self, *args, **kwargs):
+        if self._session is not None:
+            self._session.remove()
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = ScopedSession
+
+        return self._session
+
+
+celery_app.conf.update(
+    task_track_started=True, broker_connection_retry_on_startup=True
+)
