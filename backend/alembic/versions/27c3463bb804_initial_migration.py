9cd00c81733b4070824c6a9998f1d198696ceba0
Martin <alialibakhshi9898@gmail.com>
2025-05-11 06:36:32 +0100

    fix:📚  fix the alembic migration

--- backend/alembic/versions/27c3463bb804_initial_migration.py
+++ backend/alembic/versions/27c3463bb804_initial_migration.py
@@ -1,8 +1,8 @@
-"""create user table
+"""initial migration
 
-Revision ID: 5439773cff36
-Revises: 389fa78c5b29
-Create Date: 2025-04-29 16:28:13.890172
+Revision ID: 27c3463bb804
+Revises: 919c98676330
+Create Date: 2025-05-10 16:32:22.030990
 
 """
 from typing import Sequence, Union
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '5439773cff36'
-down_revision: Union[str, None] = '389fa78c5b29'
+revision: str = '27c3463bb804'
+down_revision: Union[str, None] = '919c98676330'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -72,23 +72,22 @@ def upgrade() -> None:
     op.create_index(op.f('ix_requestlog_modified'), 'requestlog', ['modified'], unique=False)
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('full_name', sa.String(length=50), nullable=False),
-    sa.Column('email', sa.String(length=50), nullable=False),
+    sa.Column('full_name', sa.String(), nullable=True),
+    sa.Column('email', sa.String(), nullable=False),
     sa.Column('hashed_password', sa.String(), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=True),
     sa.Column('is_superuser', sa.Boolean(), nullable=True),
-    sa.Column('role', sa.String(), nullable=True),
     sa.Column('is_deleted', sa.DateTime(timezone=True), nullable=True),
     sa.Column('created', sa.DateTime(timezone=True), nullable=True),
     sa.Column('modified', sa.DateTime(timezone=True), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_user_created'), 'user', ['created'], unique=False)
-    op.create_index(op.f('ix_user_email'), 'user', ['email'], unique=False)
+    op.create_index(op.f('ix_user_email'), 'user', ['email'], unique=True)
+    op.create_index(op.f('ix_user_full_name'), 'user', ['full_name'], unique=False)
     op.create_index(op.f('ix_user_id'), 'user', ['id'], unique=False)
     op.create_index(op.f('ix_user_is_deleted'), 'user', ['is_deleted'], unique=False)
     op.create_index(op.f('ix_user_modified'), 'user', ['modified'], unique=False)
-    op.create_index(op.f('ix_user_role'), 'user', ['role'], unique=False)
     op.create_table('prediction',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
@@ -118,10 +117,10 @@ def downgrade() -> None:
     op.drop_index(op.f('ix_prediction_id'), table_name='prediction')
     op.drop_index(op.f('ix_prediction_created'), table_name='prediction')
     op.drop_table('prediction')
-    op.drop_index(op.f('ix_user_role'), table_name='user')
     op.drop_index(op.f('ix_user_modified'), table_name='user')
     op.drop_index(op.f('ix_user_is_deleted'), table_name='user')
     op.drop_index(op.f('ix_user_id'), table_name='user')
+    op.drop_index(op.f('ix_user_full_name'), table_name='user')
     op.drop_index(op.f('ix_user_email'), table_name='user')
     op.drop_index(op.f('ix_user_created'), table_name='user')
     op.drop_table('user')
