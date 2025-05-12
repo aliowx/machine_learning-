2a02819c83e2cec5835ebefe4fc3cadff2e296f1
Martin <alialibakhshi9898@gmail.com>
2025-05-12 17:17:37 +0100

    feat:💾 make  the user servis

--- backend/app/api/api_v1/endpoints/user.py
+++ backend/app/api/api_v1/endpoints/user.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud, models, schemas
+from app import crud, models, schemas, utils
 from app.api import deps
 from app.api.api_v1 import services
 from app.log import log
 from app.utils import APIResponse, APIResponseType
 from app.cache import cache, invalidate
 from app.cache.util import ONE_DAY_IN_SECONDS
+from typing import Annotated
+from app import exceptions as exc
 
 router = APIRouter()
 namespace = "user"
@@ -60,3 +62,32 @@ async def update_user(
         db=db, user_id=user_id, user_in=user_in, current_user=current_user
     )
     return APIResponse(response)
+
+
+@router.get("/{user_id}")
+@cache(namespace=namespace, expire=ONE_DAY_IN_SECONDS)
+async def read_user_by_id(
+    user_id: int,
+    _: Annotated[
+        bool,
+        Depends(),
+    ],
+    current_user: models.User = Depends(deps.get_current_user_from_cookie_or_basic),
+    db: AsyncSession = Depends(deps.get_db_async),
+) -> APIResponseType[schemas.User]:
+    """
+    Get a specific user by id.
+    """
+    
+    
+    user = await crud.user.get(db=db, id=user_id)
+    
+    if not user:
+        raise exc.ServiceFailure(
+            msg_code=utils.MessageCodes.bad_request,
+            detail=" User is not define! "
+        )
+        
+        
+        
+        
