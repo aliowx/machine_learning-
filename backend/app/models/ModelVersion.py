cf0c77c0549649227c5a780dcfc8268c0c4beb39
Martin <alialibakhshi9898@gmail.com>
2025-05-18 11:04:23 +0100

    feat:⬆  improve the model ML

--- backend/app/models/ModelVersion.py
+++ backend/app/models/ModelVersion.py
@@ -7,14 +7,24 @@ from sqlalchemy import (
     Index,
     Integer,
     String,
-    func
+    func,
+    JSON
 )
 
 
 
-class ModelVersion(Base):
+class MLModel(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     
+    model_name : Mapped[str] = mapped_column(String)
+    
+    framework: Mapped[str] = mapped_column(String, nullable=False)
+    
+    inputs: Mapped[JSON] = mapped_column(JSON)
+    
+    outputs: Mapped[JSON] = mapped_column(JSON)
+    
+    preprocessing: Mapped[JSON] = mapped_column(JSON)
     version_name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     
     model_file_path: Mapped[str] = mapped_column(String, nullable=False)
@@ -23,3 +33,5 @@ class ModelVersion(Base):
         DateTime(timezone=True),
         server_default=func.now()
     )
+    
+    
