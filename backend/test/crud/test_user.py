7fd96bbb72180bcf665e97647252a85e2ce6b8fd
Martin <alialibakhshi9898@gmail.com>
2025-05-18 17:00:03 +0100

    feat:🧪 test create user

--- backend/test/crud/test_user.py
+++ backend/test/crud/test_user.py
@@ -5,5 +5,12 @@ from app import crud
 from app.core.config import settings
 from app.core.security import verify_password
 from app.schemas import UserCreate, UserBase, UserUpdate
+from test.utils.utils import random_email ,random_lower_string
 
 
+
+
+class TestUser:
+    
+    @pytest.mark.asyncio
+    async def test_user_create(self, db: AsyncSession)-> UserCreate:...
