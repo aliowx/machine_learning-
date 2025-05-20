cc40267e4a5085e6d8600f61e168b8500737eeb8
Martin <alialibakhshi9898@gmail.com>
2025-05-20 18:53:42 +0100

    test:🤔 make test Crud user

--- backend/test/crud/test_user.py
+++ backend/test/crud/test_user.py
@@ -13,4 +13,10 @@ from test.utils.utils import random_email ,random_lower_string
 class TestUser:
     
     @pytest.mark.asyncio
-    async def test_user_create(self, db: AsyncSession)-> UserCreate:...
+    async def test_user_create(self, db: AsyncSession)-> UserCreate:
+        email = random_email()
+        password = random_lower_string()
+        user_in = UserCreate(email=email, password=password)
+        user = await crud.user.create(db, obj_in=user_in)
+        assert user.email == email
+        assert hasattr(user, "hashed_password")
