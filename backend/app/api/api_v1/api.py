a98aa5c73ea358c386a2e76f9ed21e97652f17e0
Martin <alialibakhshi9898@gmail.com>
2025-05-21 15:56:19 +0100

    feat:🧪  show

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import auth, user, health, utils, upload_csv
+from app.api.api_v1.endpoints import auth, user, health, utils, upload_csv, chat_bot
 
 api_router = APIRouter()
 
@@ -8,3 +8,4 @@ api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(health.router, prefix="/health", tags=["health"])
 api_router.include_router(utils.router, prefix='/utils', tags=["utils"])
 api_router.include_router(upload_csv.router, prefix='/csv', tags=["csv"])
+api_router.include_router(chat_bot.router, prefix='/chat_bot', tags=["Chatbot"])
