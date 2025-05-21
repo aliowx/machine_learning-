170668a296ae59d87d5bc8d9368a2bd7d8dc0ba8
Martin <alialibakhshi9898@gmail.com>
2025-05-21 05:59:50 +0100

    add: 🤔 make chatbot endpoint

---
+++ backend/app/api/api_v1/endpoints/chat_bot.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI ,Request, Response, APIRouter, Depends
+from fastapi.openapi.models import OpenAPI
+from app.core.config import settings
+from app.log import log
+from app.cache import invalidate, cache
+
+
+
+
+router = APIRouter()
+
+namespace = "ChatBot"
+
+
+
+@router.get('/bot')
+# @cache()
+async def chat_bot():...
+
+
