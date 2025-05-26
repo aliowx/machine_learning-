4c517314846587533093bac16ff7a93117139550
Martin <alialibakhshi9898@gmail.com>
2025-05-26 07:25:45 +0100

    tada:🎉 feat crud chat bot

--- backend/app/crud/crud_chat_history.py
+++ backend/app/crud/crud_chat_history.py
@@ -10,4 +10,24 @@ from app.schemas.chat_bot import ChatbotCreate, ChatbotUpdate
 
 
 class CRUDChatBot(CRUDBase[ChatHistory, ChatbotCreate, ChatbotUpdate]):
-    async def create_chat_message(db: AsyncSession, chat: ChatbotCreate, bot_response:str ):pass
+    async def create_chat_message(db: AsyncSession, chat: ChatbotCreate, bot_response: str )-> ChatHistory:
+        try:
+            chat_data = jsonable_encoder(chat)
+            
+            
+            chat_data['bot_response'] = bot_response
+            
+            db_chat = ChatHistory(**chat_data)
+            
+            db.add(db_chat)
+            
+            await db.commit()
+            await db.refresh(db_chat)
+            
+            return db_chat
+        except Exception as e:
+            await db.rollback()
+            raise Exception(f"Error creating chat message: {str(e)}")
+        
+        
+        
