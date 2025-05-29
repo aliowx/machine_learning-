c1d0a913c37dd5ff20edda18e96fe516b16c134f
Martin <alialibakhshi9898@gmail.com>
2025-05-29 07:28:42 +0100

    feat:💄 crud layer fit

--- backend/app/crud/crud_chat_bot.py
+++ backend/app/crud/crud_chat_bot.py
@@ -6,12 +6,27 @@ from app.crud.base import CRUDBase
 from app.db.base_class import Base
 from app.models.chat_bot import ChatHistory
 from app.schemas.chat_bot import ChatbotCreate, ChatbotUpdate, Chatbot
+from typing import Optional 
+import exceptions as exc
+from app.utils import MessageCodes
+import logging
 
 
+logger = logging.getLogger(__name__)
 
 class CRUDChatBot(CRUDBase[ChatHistory, ChatbotCreate, ChatbotUpdate]):
-    async def create_chat_message(self, db: AsyncSession, chat: ChatbotCreate, bot_response: str )-> ChatHistory:
+    async def create_chat_message(self, db: AsyncSession, chat: ChatbotCreate, bot_response: str, user_id: Optional[int]=None )-> ChatHistory:
         try:
+            if not chat.message.split():
+                raise exc.AlreadyExistException(
+                    detail='Message cannot be empty',
+                    msg_code=MessageCodes.bad_request
+                )
+            if not bot_response.split():
+                raise exc.AlreadyExistException(
+                    detail='Bot response cannot empty',
+                    msg_code=MessageCodes.bad_request
+                )
             chat_data = jsonable_encoder(chat)
             
             
